bdd6dad65c8448fb6f81d758cf16fdb22a486042
rubayte <rubayte.iut@gmail.com>
2013-07-15 14:44:13 +0200

    geevsapp initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GEEVSapp::Application.config.secret_token = 'dcb58c54f040953f781d713e9fcbe8586e8bb519d35214c56dd45df177b420f3f9e72082b231c70adfa1589023b597086338658dd69191243f221b13b0b1e43a'
